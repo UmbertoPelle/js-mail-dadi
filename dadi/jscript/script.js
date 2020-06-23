41b83d255790f76552a7423b18716363a7f5ac00
Umberto <umbertopellegrino96@gmail.com>
2020-06-23 16:49:06 +0200

    Divisione esercizi

--- dadi/jscript/script.js
+++ dadi/jscript/script.js
@@ -17,27 +17,3 @@ btnDadi.addEventListener("click", function () {
   }
 
 });
-
-//Per quello delle mail far inserire all'utente una mail e controllare se è nell'array, che contiene quelle valide.
-//Dare all'utente un messaggio opportuno.
-var btnEmail = document.getElementById('btnEmail');
-var email = ["umberto@gmail.com", "mario@gmail.com", "ugo@gmail.com"];
-var emailResult = document.getElementById('emailResult');
-
-btnEmail.addEventListener("click",function () {
-  var emailUtente = document.getElementById('mainEmail').value;
-  var emailTrovata = false;
-
-  for (var i = 0; i < email.length; i++) {
-    if (emailUtente == email[i]) {
-      emailTrovata = true;
-    }
-  }
-
-  if (emailTrovata == true) {
-    emailResult.innerHTML = "la tua email è presente";
-  } else {
-    emailResult.innerHTML = "La tua email è assente";
-  }
-
-});
